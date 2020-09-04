aba069efe651c13f09f853eae52a63c70689928e
Blyablin Egor Alexeevich <eg.blyablin@yandex.ru>
2020-09-04 21:11:59 +0300

    pycache remove

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ developers = ["Egor Blyablin"]
 activity_type = "discord.ActivityType.playing"
 activity_name = f"{prefix}help"
 activity_emoji = None
-TOKEN = 'NzUwMjkyODU3MzM1NTc4NjI0.X04arA.1AdiJu4p3rw5xh7086w6klHpOjw'
+TOKEN = 'NzUwMjkyODU3MzM1NTc4NjI0.X04arA.lwjQmmJCYtWpGAvR64dnqIBdtEQ'
