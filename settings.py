e2eff1eefd5f22f329ddf161abaff9fb7415afe3
Blyablin Egor Alexeevich <60047296+EgorkA82@users.noreply.github.com>
2020-09-05 18:13:19 +0300

    .gitignore changes - settings.py add

---
+++ settings.py
@@ -0,0 +1,6 @@
+current_version = 0.3
+prefix = "/"
+developers = ["Egor Blyablin"]
+activity_name = f"{prefix}help"
+activity_emoji = None
+TOKEN = 'NzUwMjkyODU3MzM1NTc4NjI0.X04arA.2BdJxswRNpbStV4_-SzTD-CfjXY'
