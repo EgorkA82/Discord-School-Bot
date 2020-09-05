39d8eb583b03768448423bfce3a8f76a868ec642
Blyablin Egor Alexeevich <60047296+EgorkA82@users.noreply.github.com>
2020-09-05 18:15:22 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ prefix = "/"
 developers = ["Egor Blyablin"]
 activity_name = f"{prefix}help"
 activity_emoji = None
-TOKEN = 'NzUwMjkyODU3MzM1NTc4NjI0.X04arA.2BdJxswRNpbStV4_-SzTD-CfjXY'
+TOKEN = ''
